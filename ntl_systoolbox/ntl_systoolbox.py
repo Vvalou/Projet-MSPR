3c786c4e8b0c94f55fdd242b58064154e7e9040f
Vvalou <vergult.valentin34@gmail.com>
2025-11-04 17:19:26 +0100

    Ajout des fonctions utilitaires : clear_screen, print_title, pause

--- ntl_systoolbox/ntl_systoolbox.py
+++ ntl_systoolbox/ntl_systoolbox.py
@@ -4,3 +4,14 @@ import os
 
 if __name__ == "__main__":
     print("NTL-SysToolbox - Script en cours de développement")
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def print_title(text):
+    print("="*60)
+    print(text.center(60))
+    print("="*60)
+
+def pause():
+    input("\nAppuyez sur Entrée pour continuer...")
