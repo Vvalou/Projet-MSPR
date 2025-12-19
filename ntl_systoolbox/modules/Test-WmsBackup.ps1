2f05379d4c2e5f9e58a89fa33ee4b4eae296be8d
Wiksem <110357246+Wiksem@users.noreply.github.com>
2025-12-19 11:03:32 +0100

    Ajout de script
    Ajout de script précedemment supprimé avec tous les autres modules

---
+++ ntl_systoolbox/modules/Test-WmsBackup.ps1
@@ -0,0 +1,30 @@
+while ($true) {
+    Clear-Host
+    Write-Host "=== TEST MODULE SAUVEGARDE WMS ===`n"
+    Write-Host "1) Afficher le contenu de la table inventory"
+    Write-Host "2) Tester la sauvegarde SQL complete"
+    Write-Host "3) Tester l'export CSV de la table inventory"
+    Write-Host "0) Quitter`n"
+
+    $choice = Read-Host "Votre choix"
+
+    switch ($choice) {
+        "1" {
+            docker exec -it wms-db mysql -uwms_backup -pBackupWMS2025 wms -e "SELECT * FROM inventory;"
+            Pause
+        }
+        "2" {
+            powershell -File .\src\Backup-Wms.ps1 -Mode Full
+            Pause
+        }
+        "3" {
+            powershell -File .\src\Backup-Wms.ps1 -Mode Table -TableName inventory
+            Pause
+        }
+        "0" { break }
+        default {
+            Write-Host "Choix invalide."
+            Pause
+        }
+    }
+}
