d0fede297261eeff440c02caabc89c23c28b6bd7
Vvalou <160333758+Vvalou@users.noreply.github.com>
2025-12-19 11:18:21 +0100

    02 Ajout des fonctions d'affichage

--- ntl_systoolbox/modules/systeme_windows.py
+++ ntl_systoolbox/modules/systeme_windows.py
@@ -18,3 +18,21 @@ ROUGE = '\033[91m'
 BLEU = '\033[94m'
 CYAN = '\033[96m'
 BOLD = '\033[1m'
+
+def titre(texte):
+    """Affiche un titre en cyan"""
+    print("\n" + CYAN + BOLD + "=" * 60)
+    print(texte.center(60))
+    print("=" * 60 + RESET)
+
+def ok(texte):
+    """Affiche ✓ en vert"""
+    print(VERT + "  ✓ " + RESET + texte)
+
+def erreur(texte):
+    """Affiche ✗ en rouge"""
+    print(ROUGE + "  ✗ " + RESET + texte)
+
+def info(texte):
+    """Affiche une info en bleu"""
+    print(BLEU + texte + RESET)
